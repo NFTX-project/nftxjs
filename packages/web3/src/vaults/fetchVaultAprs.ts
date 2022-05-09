9c28a196ccaabe7860c54048f908de7443bb295e
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2022-05-09 13:26:20 +0100

    feat(@nftx/config): new package

--- packages/web3/src/vaults/fetchVaultAprs.ts
+++ packages/web3/src/vaults/fetchVaultAprs.ts
@@ -1,18 +1,33 @@
+import { NFTX_APR_URL } from '@nftx/constants';
+import { addressEqual } from '../web3';
 import type { VaultAddress, VaultApr } from './types';
+
+type Response = Array<{
+  vault_id: string;
+  inventoryApr: Record<string, string>;
+  liquidityApr: Record<string, string>;
+}>;
 
 const fetchVaultAprs = async ({
   vaultAddresses,
 }: {
   vaultAddresses: VaultAddress[];
 }): Promise<VaultApr[]> => {
-  // TODO: implement this
-  console.warn('fetchVaultAprs is currently stubbed');
+  const response = await fetch(NFTX_APR_URL);
+  let data: Response;
+  try {
+    data = await response.json();
+  } catch {
+    data = [];
+  }
+
   return vaultAddresses.map((vaultAddress): VaultApr => {
+    const x = data.find(({ vault_id }) => addressEqual(vault_id, vaultAddress));
+
     return {
       vaultAddress,
-      vaultId: '0',
-      liquidityApr: Math.floor(Math.random() * 300) / 100,
-      inventoryApr: Math.floor(Math.random() * 100) / 100,
+      liquidityApr: Number(Object.values(x?.liquidityApr ?? {})[0] ?? '0'),
+      inventoryApr: Number(Object.values(x?.inventoryApr ?? {})[0] ?? '0'),
     };
   });
 };
