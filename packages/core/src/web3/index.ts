14ecbbb88a7620814b0c23cecc8a3704d79c5c48
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2022-06-29 10:27:06 +0100

    feat: fromEhtersNumber

--- packages/core/src/web3/index.ts
+++ packages/core/src/web3/index.ts
@@ -2,6 +2,7 @@ export { default as addressEqual } from './addressEqual';
 export { default as fetchTokenBalance } from './fetchTokenBalance';
 export { default as fetchTotalSupply } from './fetchTotalSupply';
 export { default as getChainConstant } from './getChainConstant';
+export { default as fromEthersNumber } from './fromEthersNumber';
 export { default as getContract } from './getContract';
 export { default as MulticallContract } from './MulticallContract';
 export { default as parseLogEvent } from './parseLogEvent';
--- packages/core/src/web3/toEthersNumber.ts
+++ packages/core/src/web3/toEthersNumber.ts
@@ -3,7 +3,10 @@ import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
 /** Takes a BigNumber and returns a regular number
  * Essentially the same as formatEther but without the formatting
  */
-const toEthersNumber = (val: BigNumberish, precision = 18) => {
+const toEthersNumber = (
+  val: BigNumberish,
+  { precision = 18 }: { precision?: number } = {}
+) => {
   if (val == null) {
     return null;
   }
