6168a0de6043bc3efd8f58acff14ad7c683dc239
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2023-11-28 12:27:33 +0000

    feat(@nftx/core): fetchPremiumPaids method to get all premium fees paid out

---
+++ packages/core/src/pools/fetchPremiumPaids.ts
@@ -0,0 +1,68 @@
+import { NftxV3, type Address } from '@nftx/types';
+import { getChainConstant } from '@nftx/utils';
+import { createQuery, querySubgraph } from '@nftx/subgraph';
+import config from '@nftx/config';
+
+const fetchPremiumPaids = async ({
+  network,
+  vaultAddresses,
+  vaultIds,
+}: {
+  network: number;
+  vaultAddresses?: Address[];
+  vaultIds?: string[];
+}) => {
+  const premiumPaids: {
+    date: number;
+    amount: bigint;
+    vaultId: string;
+    to: Address;
+    vaultAddress: Address;
+  }[] = [];
+  let lastId: string | undefined;
+
+  do {
+    const query = createQuery<NftxV3.Query>()
+      .premiumPaids.orderBy('id')
+      .orderDirection('asc')
+      .first(1000)
+      .where((w) => [
+        w.id.gt(lastId),
+        w.vault.in(vaultAddresses),
+        w.vault((vault) => [vault.vaultId.in(vaultIds)]),
+      ])
+      .select((s) => [
+        s.amount,
+        s.date,
+        s.id,
+        s.to((to) => [to.id]),
+        s.vault((vault) => [vault.id, vault.vaultId]),
+      ]);
+    const data = await querySubgraph({
+      query,
+      url: getChainConstant(config.subgraph.NFTX_SUBGRAPH, network),
+    });
+
+    premiumPaids.push(
+      ...data.premiumPaids.map((p) => {
+        return {
+          date: Number(p.date),
+          amount: BigInt(p.amount),
+          vaultId: p.vault.vaultId,
+          to: p.to.id as Address,
+          vaultAddress: p.vault.id as Address,
+        };
+      })
+    );
+
+    if (data.premiumPaids.length === 1000) {
+      lastId = data.premiumPaids[data.premiumPaids.length - 1].id;
+    } else {
+      lastId = undefined;
+    }
+  } while (lastId);
+
+  return premiumPaids;
+};
+
+export default fetchPremiumPaids;
--- packages/core/src/pools/index.ts
+++ packages/core/src/pools/index.ts
@@ -2,3 +2,4 @@ export { default as fetchLiquidityPool } from './fetchLiquidityPool';
 export { default as fetchLiquidityPools } from './fetchLiquidityPools';
 export { default as fetchInventoryPool } from './fetchInventoryPool';
 export { default as fetchInventoryPools } from './fetchInventoryPools';
+export { default as fetchPremiumPaids } from './fetchPremiumPaids';
