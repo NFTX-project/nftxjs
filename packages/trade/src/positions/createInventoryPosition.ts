b6701c38c9e05a84a0d3c9d5905b936f57292c29
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2023-12-08 21:38:51 +0000

    feat: create inventory position with vToken

--- packages/trade/src/positions/createInventoryPosition.ts
+++ packages/trade/src/positions/createInventoryPosition.ts
@@ -1,21 +1,35 @@
 import { InventoryStaking } from '@nftx/abi';
 import config from '@nftx/config';
 import { INVENTORY_STAKING } from '@nftx/constants';
-import { CreateInventoryPositionQuote, Provider, Signer } from '@nftx/types';
+import {
+  CreateInventoryPositionQuote,
+  Permit2Quote,
+  Provider,
+  Signer,
+} from '@nftx/types';
 import { getChainConstant, getContract } from '@nftx/utils';
 
 const createInventoryPosition = ({
   provider,
   quote: {
-    methodParameters: { nftAmounts, nftIds, userAddress, vaultId },
+    methodParameters: {
+      nftAmounts,
+      nftIds,
+      userAddress,
+      vaultId,
+      vToken,
+      usePermit2,
+    },
   },
   signer,
   network = config.network,
+  permit2,
 }: {
   network?: number;
   quote: Pick<CreateInventoryPositionQuote, 'methodParameters'>;
   provider: Provider;
   signer: Signer;
+  permit2?: Permit2Quote;
 }) => {
   const contract = getContract({
     address: getChainConstant(INVENTORY_STAKING, network),
@@ -23,6 +37,20 @@ const createInventoryPosition = ({
     abi: InventoryStaking,
     signer,
   });
+
+  // You can either dpeosit with vToken, or with NFTs
+  if (vToken) {
+    return contract.write.deposit({
+      args: [
+        BigInt(vaultId),
+        vToken,
+        userAddress,
+        (usePermit2 ? permit2?.permit2encoded : undefined) || '0x',
+        !!(usePermit2 && permit2),
+        true,
+      ],
+    });
+  }
 
   return contract.write.depositWithNFT({
     args: [BigInt(vaultId), nftIds, nftAmounts, userAddress],
