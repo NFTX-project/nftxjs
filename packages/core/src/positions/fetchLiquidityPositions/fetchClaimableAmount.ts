7c0250c9ca0bff44d163a1bc21dfda6b926afc5d
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2023-12-12 12:48:17 +0000

    fix: recover from unclaimable positions
    if a position has no claimable amounts and is out of range (eth-only or vtoken-only) then estimating the claimable amount via contract calls will fail.
    Rather than throwing and completely failing to fetch the position, we now just return 0 for the claimable amount.

--- packages/core/src/positions/fetchLiquidityPositions/fetchClaimableAmount.ts
+++ packages/core/src/positions/fetchLiquidityPositions/fetchClaimableAmount.ts
@@ -1,5 +1,5 @@
 import { NonfungiblePositionManager } from '@nftx/abi';
-import { NONFUNGIBLE_POSITION_MANAGER } from '@nftx/constants';
+import { NONFUNGIBLE_POSITION_MANAGER, Zero } from '@nftx/constants';
 import type { Provider, TokenId } from '@nftx/types';
 import { getChainConstant } from '@nftx/utils';
 
@@ -14,30 +14,34 @@ const fetchClaimableAmount = async ({
   network: number;
   tokenId: TokenId;
 }): Promise<[bigint, bigint]> => {
-  const ownerAddress = await provider.readContract({
-    abi: NonfungiblePositionManager,
-    address: getChainConstant(NONFUNGIBLE_POSITION_MANAGER, network),
-    functionName: 'ownerOf',
-    args: [BigInt(tokenId)],
-  });
+  try {
+    const ownerAddress = await provider.readContract({
+      abi: NonfungiblePositionManager,
+      address: getChainConstant(NONFUNGIBLE_POSITION_MANAGER, network),
+      functionName: 'ownerOf',
+      args: [BigInt(tokenId)],
+    });
 
-  const {
-    result: [amount0, amount1],
-  } = await provider.simulateContract({
-    abi: NonfungiblePositionManager,
-    address: getChainConstant(NONFUNGIBLE_POSITION_MANAGER, network),
-    functionName: 'collect',
-    args: [
-      {
-        amount0Max: MAX_UINT128,
-        amount1Max: MAX_UINT128,
-        recipient: ownerAddress,
-        tokenId: BigInt(tokenId),
-      },
-    ],
-  });
+    const {
+      result: [amount0, amount1],
+    } = await provider.simulateContract({
+      abi: NonfungiblePositionManager,
+      address: getChainConstant(NONFUNGIBLE_POSITION_MANAGER, network),
+      functionName: 'collect',
+      args: [
+        {
+          amount0Max: MAX_UINT128,
+          amount1Max: MAX_UINT128,
+          recipient: ownerAddress,
+          tokenId: BigInt(tokenId),
+        },
+      ],
+    });
 
-  return [amount0, amount1];
+    return [amount0, amount1];
+  } catch (e) {
+    return [Zero, Zero];
+  }
 };
 
 export default fetchClaimableAmount;
