9b6cde06529fe8456df07d4c4fe9b6cbe5d4b28a
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2024-01-03 12:36:59 +0000

    fix: update latest contract addresses

--- packages/constants/src/addresses.ts
+++ packages/constants/src/addresses.ts
@@ -34,45 +34,46 @@ export const OPENSEA_COLLECTION = a(
   '0x495f947276749ce646f68ac8c248420045cb7b5e'
 );
 
-/** Zap specifically for creating new vaults */
+/** Zap specifically for creating new vaults - CreateVaultZap */
 export const CREATE_VAULT_ZAP = {
-  [Network.Mainnet]: a('0x510c9fB7d1ab8b0338B7434872f0D22633587A0d'),
+  [Network.Mainnet]: a('0x56dab32697B4A313f353DA0CE42B5113eD8E6f74'),
   [Network.Goerli]: a('0xd58A1627bF1C2F8C724A9493A979C0369ab17AaB'),
-  [Network.Sepolia]: a('0xE7d5d168A8812Dcbe372A44a7632d6156f4DB13b'),
+  [Network.Sepolia]: a('0xD80b916470F8e79FD8d09874cb159CbB8D13d8da'),
 };
 
-/** Perform buy/sell/swaps */
+/** Perform buy/sell/swaps - MarketplaceUniversalRouterZap */
 export const MARKETPLACE_ZAP = {
   [Network.Mainnet]: a('0x293A0c49c85F1D8851C665Ac3cE1f1DC2a79bE3d'),
   [Network.Goerli]: a('0x0be2D766Eef4b6a72F1fAe2e49619F013d647B8A'),
   [Network.Sepolia]: a('0xd88a3B9D0Fb2d39ec8394CfFD983aFBB2D4a6410'),
 };
 
-/** Stake NFTs or vTokens for xNFTs */
+/** Stake NFTs or vTokens for xNFTs - NFTXInventoryStakingV3Upgradable */
 export const INVENTORY_STAKING = {
   [Network.Mainnet]: a('0x889f313e2a3FDC1c9a45bC6020A8a18749CD6152'),
   [Network.Goerli]: a('0xEf771a17e6970d8B4b208a76e94F175277554230'),
   [Network.Sepolia]: a('0xfBFf0635f7c5327FD138E1EBa72BD9877A6a7C1C'),
 };
 
-/** AMM Router (Universal Router / nftxUniversalRouter) */
+/** AMM Router - nftxUniversalRouter */
 export const NFTX_ROUTER = {
   [Network.Mainnet]: a('0x250d62a67254A46c0De472d2c9215E1d890cC90f'),
   [Network.Goerli]: a('0xF7c4FC5C2e30258e1E4d1197fc63aeDE371508f3'),
   [Network.Sepolia]: a('0x12156cCA1958B6591CC49EaE03a5553458a4b424'),
 };
 
-/** Staking  / Liquidity / Pools (NftxRouter) */
+/** Staking  / Liquidity / Pools - NftxRouter */
 export const POOL_ROUTER = {
-  [Network.Mainnet]: a('0x833657427efF3365d768202B986fe1164De13a94'),
+  [Network.Mainnet]: a('0x70A741A12262d4b5Ff45C0179c783a380EebE42a'),
   [Network.Goerli]: a('0xb95e2Fb1eDA32BbFbDaE2463BB3E64bb3E1E41D5'),
-  [Network.Sepolia]: a('0xD36ece08F76c50EC3F01db65BBc5Ef5Aa5fbE849'),
+  [Network.Sepolia]: a('0x441b7DE4340AAa5aA86dB4DA43d9Badf7B2DAA66'),
 };
 
+/** NonfungiblePositionManager */
 export const NONFUNGIBLE_POSITION_MANAGER = {
-  [Network.Mainnet]: a('0xb38b19B06D96f1C0A3bC3a1478613ba91f072fda'),
+  [Network.Mainnet]: a('0x26387fcA3692FCac1C1e8E4E2B22A6CF0d4b71bF'),
   [Network.Goerli]: a('0xaEC6537206e8e590603399c714c39947680f1181'),
-  [Network.Sepolia]: a('0x55BDc76262B1e6e791D0636A0bC61cee23CDFa87'),
+  [Network.Sepolia]: a('0xA9bCC1e29d3460177875f68fDCC0264D22c40BF0'),
 };
 
 /** The permit2 contract, needs an approval signature because the UniversalRouter only transfers tokens from user via Permit2 */
@@ -90,12 +91,14 @@ export const ENS_REGISTRAR = {
   [Network.Sepolia]: a('0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85'),
 };
 
+/** UniswapV3FactoryUpgradeable */
 export const AMM_FACTORY = {
   [Network.Mainnet]: a('0xa70e10beB02fF9a44007D9D3695d4b96003db101'),
   [Network.Goerli]: a('0xf25081B098c5929A26F562aa2502795fE89BC73f'),
   [Network.Sepolia]: a('0xDD2dce9C403f93c10af1846543870D065419E70b'),
 };
 
+/** NFTXVaultFactoryUpgradableV2 */
 export const VAULT_FACTORY = {
   [Network.Mainnet]: a('0xC255335bc5aBd6928063F5788a5E420554858f01'),
   [Network.Goerli]: a('0x1d552A0e6c2f680872C4a88b1e7def05F1858dF0'),
