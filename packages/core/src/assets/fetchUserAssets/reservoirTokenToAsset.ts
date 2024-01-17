042a707cc8a2de33d8f129e5e7294eca11aa0fae
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2024-01-17 10:17:58 +0000

    fix: imageUrl should fallback to media property

--- packages/core/src/assets/fetchUserAssets/reservoirTokenToAsset.ts
+++ packages/core/src/assets/fetchUserAssets/reservoirTokenToAsset.ts
@@ -19,17 +19,20 @@ const reservoirTokenToAsset = ({
   };
   vaultIds: string[];
 }) => {
+  const imageUrl = token.image || token.imageSmall || token.media;
+  const animationUrl = token.media?.includes('.mp4') ? token.media : undefined;
+
   const asset: Asset = {
     assetAddress: token.contract,
     tokenId: token.tokenId,
     id: `${token.contract}/${token.tokenId}`,
     imagePreviewUrl: token.imageSmall,
-    imageUrl: token.image,
+    imageUrl,
     isFlagged: !!token.isFlagged,
     name: token.name,
     rarity: token.rarityScore,
     rarityRank: token.rarityRank,
-    animationUrl: token.media?.includes('.mp4') ? token.media : undefined,
+    animationUrl,
     quantity: token.supply ? BigInt(token.supply) : undefined,
     traits: (token.attributes ?? []).reduce((acc, { key, value }) => {
       return { ...acc, [key]: value };
