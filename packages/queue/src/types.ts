0698950191e9edb6a37e0e6f07a60a146246ffe6
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2024-01-25 11:25:01 +0000

    feat: new queue signals

--- packages/queue/src/types.ts
+++ packages/queue/src/types.ts
@@ -4,15 +4,20 @@ export type Signals = {
   'index-collection': { collectionAddress: Address; network: number };
   'index-vault': { network: number; vaultId: string };
   'index-inactive-vaults': { network: number };
+  'index-new-vaults': { network: number };
+  'index-twap-vaults': { network: number };
 };
 
 type ListenerFn<T, S extends keyof T> = (signal: {
   type: S;
   payload: T[S];
 }) => any;
+
 export type SignalCallback = ListenerFn<Signals, 'index-vault'> &
   ListenerFn<Signals, 'index-collection'> &
-  ListenerFn<Signals, 'index-inactive-vaults'>;
+  ListenerFn<Signals, 'index-inactive-vaults'> &
+  ListenerFn<Signals, 'index-new-vaults'> &
+  ListenerFn<Signals, 'index-twap-vaults'>;
 
 export type Messages = {
   'collection-indexed': { collectionAddress: Address; network: number };
