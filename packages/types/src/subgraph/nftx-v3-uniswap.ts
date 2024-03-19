f528f77cea803acd6adfdd041cda9026b346a2c2
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2024-03-19 10:21:25 +0000

    refactor: combine cross-chain types for the 721 and 1155 subgraphs
    we were previous exporting an ERC721 and ERC1155 subgraph schema for each chain
    this was a legacy pattern as each chain had a completely different format
    however, we now have a consistent schema across all chains
    this commit combines ERC721Mainnet and ERC721Sepolia into a single ERC721 schema
    this commit combines ERC1155Mainnet and ERC1155Sepolia into a single ERC1155 schema
    
    BREAKING CHANGE: any imports of the 721 or 1155 subgraph schemas will need to be updated

--- packages/types/src/subgraph/nftx-v3-uniswap.ts
+++ packages/types/src/subgraph/nftx-v3-uniswap.ts
@@ -186,6 +186,11 @@ export enum ActiveAccount_OrderBy {
   Id = 'id'
 }
 
+export enum Aggregation_Interval {
+  Day = 'day',
+  Hour = 'hour'
+}
+
 export type BlockChangedFilter = {
   number_gte: Scalars['Int']['input'];
 };
@@ -7983,6 +7988,8 @@ export type _Block_ = {
   hash?: Maybe<Scalars['Bytes']['output']>;
   /** The block number */
   number: Scalars['Int']['output'];
+  /** The hash of the parent block */
+  parentHash?: Maybe<Scalars['Bytes']['output']>;
   /** Integer representation of the timestamp stored in blocks for the chain */
   timestamp?: Maybe<Scalars['Int']['output']>;
 };
