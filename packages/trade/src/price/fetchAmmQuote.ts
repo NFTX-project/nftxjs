5bab97a4f25746baf8e9b0d953ac23e7c44900c0
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2024-06-04 10:49:17 +0100

    feat(@nftx/trade): add option to soft fail out of amm quotes
    By default if an amm quote fails or is invalid, it will throw an error
    This commit adds the option to instead simply return an empty quote object

--- packages/trade/src/price/fetchAmmQuote.ts
+++ packages/trade/src/price/fetchAmmQuote.ts
@@ -6,7 +6,7 @@ import type {
   QuoteToken,
 } from '@nftx/types';
 import config from '@nftx/config';
-import { NFTX_ROUTER, WeiPerEther } from '@nftx/constants';
+import { NFTX_ROUTER, WeiPerEther, Zero } from '@nftx/constants';
 import { getChainConstant } from '@nftx/utils';
 import parseQuoteToken from './parseQuoteToken';
 import {
@@ -17,15 +17,31 @@ import {
 import { NftxQuote } from './types';
 import ammQuoteToPrice from './ammQuoteToPrice';
 
+const fallbackQuote = {
+  approveContracts: [],
+  estimatedGas: Zero,
+  feePrice: Zero,
+  items: [],
+  methodParameters: undefined as any,
+  premiumPrice: Zero,
+  price: Zero,
+  vTokenPrice: Zero,
+  type: 'erc20' as const,
+};
+
 const fetchAmmQuote = async (args: {
   network?: number;
   buyToken: QuoteToken;
   buyAmount?: BigIntish;
   sellToken: QuoteToken;
   sellAmount?: BigIntish;
+  /** The amount of slippage (in decimal terms) you're willing to accept - defaults to 1% */
   slippagePercentage?: number;
   userAddress?: Address;
+  /** Optionally use permit2 to approve spending tokens */
   permit2?: Permit2Quote;
+  /** If unable to get a quote, this option determines whether to throw an error, or just return a 0 price object. Defaults to true */
+  throwOnError?: boolean;
 }): Promise<MarketplaceQuote> => {
   const {
     network = config.network,
@@ -34,95 +50,109 @@ const fetchAmmQuote = async (args: {
     slippagePercentage = 0.01,
     userAddress,
     permit2,
+    throwOnError = true,
   } = args;
-  const sellToken = parseQuoteToken(args.sellToken, network);
-  const buyToken = parseQuoteToken(args.buyToken, network);
 
-  const baseUrl = getChainConstant(config.urls.NFTX_ROUTER_URL, network, null);
+  try {
+    const sellToken = parseQuoteToken(args.sellToken, network);
+    const buyToken = parseQuoteToken(args.buyToken, network);
 
-  ValidationError.validate({
-    sellToken: () => !!sellToken || 'Required',
-    network: () => {
-      if (!network) {
-        return 'Required';
-      }
-      if (!baseUrl) {
-        return 'Not currently supported for NFTX Router';
-      }
-    },
-    buyToken: () => !!buyToken || 'Required',
-  });
-
-  const searchParams = new URLSearchParams();
-  searchParams.append('tokenInAddress', sellToken);
-  searchParams.append('tokenInChainId', `${network}`);
-  searchParams.append('tokenOutAddress', buyToken);
-  searchParams.append('tokenOutChainId', `${network}`);
-
-  if (buyAmount) {
-    searchParams.append('amount', BigInt(buyAmount).toString());
-    searchParams.append('type', 'exactOut');
-  } else if (sellAmount) {
-    searchParams.append('amount', BigInt(sellAmount).toString());
-    searchParams.append('type', 'exactIn');
-  } else {
-    // Default to just buying 1
-    searchParams.append('buyAmount', WeiPerEther.toString());
-    searchParams.append('type', 'exactOut');
-  }
-
-  if (userAddress) {
-    searchParams.append('recipient', userAddress);
-    searchParams.append('deadline', '300');
-    searchParams.append('enableUniversalRouter', 'true');
-    searchParams.append(
-      'slippageTolerance',
-      `${(slippagePercentage ?? 0.01) * 100}`
+    const baseUrl = getChainConstant(
+      config.urls.NFTX_ROUTER_URL,
+      network,
+      null
     );
-    searchParams.append('intent', 'swap');
-  } else {
-    searchParams.append('intent', 'quote');
+
+    ValidationError.validate({
+      sellToken: () => !!sellToken || 'Required',
+      network: () => {
+        if (!network) {
+          return 'Required';
+        }
+        if (!baseUrl) {
+          return 'Not currently supported for NFTX Router';
+        }
+      },
+      buyToken: () => !!buyToken || 'Required',
+    });
+
+    const searchParams = new URLSearchParams();
+    searchParams.append('tokenInAddress', sellToken);
+    searchParams.append('tokenInChainId', `${network}`);
+    searchParams.append('tokenOutAddress', buyToken);
+    searchParams.append('tokenOutChainId', `${network}`);
+
+    if (buyAmount) {
+      searchParams.append('amount', BigInt(buyAmount).toString());
+      searchParams.append('type', 'exactOut');
+    } else if (sellAmount) {
+      searchParams.append('amount', BigInt(sellAmount).toString());
+      searchParams.append('type', 'exactIn');
+    } else {
+      // Default to just buying 1
+      searchParams.append('buyAmount', WeiPerEther.toString());
+      searchParams.append('type', 'exactOut');
+    }
+
+    if (userAddress) {
+      searchParams.append('recipient', userAddress);
+      searchParams.append('deadline', '300');
+      searchParams.append('enableUniversalRouter', 'true');
+      searchParams.append(
+        'slippageTolerance',
+        `${(slippagePercentage ?? 0.01) * 100}`
+      );
+      searchParams.append('intent', 'swap');
+    } else {
+      searchParams.append('intent', 'quote');
+    }
+
+    searchParams.append('protocols', 'v3');
+
+    if (permit2) {
+      searchParams.append('permitSignature', permit2.signature);
+      searchParams.append('permitAmount', permit2.amount.toString());
+      searchParams.append('permitExpiration', permit2.expiration.toString());
+      searchParams.append('permitSigDeadline', permit2.sigDeadline.toString());
+      searchParams.append('permitNonce', permit2.nonce.toString());
+    }
+
+    const query = searchParams.toString();
+
+    const url = `${baseUrl}?${query}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      const json = await response.json();
+      throw new QuoteFailedError(json);
+    }
+
+    const data: NftxQuote = await response.json();
+
+    if (userAddress && !data?.methodParameters) {
+      throw new QuoteSlippageError();
+    }
+    if (userAddress && config.debug) {
+      console.debug(data);
+    }
+
+    if (data?.methodParameters?.to) {
+      // We need to override the to property as it's pointing to the Uniswap router instead of our own
+      data.methodParameters.to = getChainConstant(NFTX_ROUTER, network);
+    }
+
+    return ammQuoteToPrice({
+      ...data,
+      network,
+      sellToken,
+      buyToken,
+    });
+  } catch (e) {
+    if (!throwOnError) {
+      return fallbackQuote;
+    }
+
+    throw e;
   }
-
-  searchParams.append('protocols', 'v3');
-
-  if (permit2) {
-    searchParams.append('permitSignature', permit2.signature);
-    searchParams.append('permitAmount', permit2.amount.toString());
-    searchParams.append('permitExpiration', permit2.expiration.toString());
-    searchParams.append('permitSigDeadline', permit2.sigDeadline.toString());
-    searchParams.append('permitNonce', permit2.nonce.toString());
-  }
-
-  const query = searchParams.toString();
-
-  const url = `${baseUrl}?${query}`;
-  const response = await fetch(url);
-  if (!response.ok) {
-    const json = await response.json();
-    throw new QuoteFailedError(json);
-  }
-
-  const data: NftxQuote = await response.json();
-
-  if (userAddress && !data?.methodParameters) {
-    throw new QuoteSlippageError();
-  }
-  if (userAddress && config.debug) {
-    console.debug(data);
-  }
-
-  if (data?.methodParameters?.to) {
-    // We need to override the to property as it's pointing to the Uniswap router instead of our own
-    data.methodParameters.to = getChainConstant(NFTX_ROUTER, network);
-  }
-
-  return ammQuoteToPrice({
-    ...data,
-    network,
-    sellToken,
-    buyToken,
-  });
 };
 
 export default fetchAmmQuote;
