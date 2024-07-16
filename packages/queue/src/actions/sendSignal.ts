3e0e1dce47dbd4049ca47398eea6463b902233fa
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2024-07-16 13:11:17 +0100

    fix(@nftx/queue): generate custom job ids
    if a job is emitted multiple times, it gets picked up multiple times as well
    there aren't any signals that would need to be worked on multiple times simultaneously
    to avoid this, we now generate the jobId using the signal data, bullmq will automatically
    de-dupe jobs with the same id so this should help reduce the number of duplicate jobs

--- packages/queue/src/actions/sendSignal.ts
+++ packages/queue/src/actions/sendSignal.ts
@@ -1,6 +1,23 @@
 import { getSignalQueue } from '../queues';
 import { Signals } from '../types';
 
+const createJobId = <S extends keyof Signals>({
+  payload,
+  type,
+}: {
+  type: S;
+  payload: Signals[S];
+}) => {
+  const params = Object.entries(payload)
+    .sort(([a], [b]) => a.localeCompare(b))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
+
+  const header = `${type}__`;
+
+  return `${header}${params}`.replace(/:/g, ''); // jobId cannot contain colons
+};
+
 const sendSignal = <S extends keyof Signals>({
   payload,
   type,
@@ -8,8 +25,9 @@ const sendSignal = <S extends keyof Signals>({
   type: S;
   payload: Signals[S];
 }) => {
+  const jobId = createJobId({ type, payload });
   const queue = getSignalQueue();
-  return queue.add(type, payload);
+  return queue.add(type, payload, { jobId });
 };
 
 export default sendSignal;
